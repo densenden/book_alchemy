55459e5b0580863dfda91b82e67102b62a0678b1
densenden <git@deniskreuzer.dk>
2025-03-10 17:06:40 +0100

    added add author route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
 
@@ -17,3 +17,33 @@ if not os.path.exists(db_dir):
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 db.init_app(app)
+
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        try:
+            # Parse form data
+            name = request.form['name']
+            birth_date = datetime.strptime(request.form['birthdate'], '%Y-%m-%d').date()
+            date_of_death = None
+            if request.form['date_of_death']:
+                date_of_death = datetime.strptime(request.form['date_of_death'], '%Y-%m-%d').date()
+
+            # Create new author
+            new_author = Author(
+                name=name,
+                birth_date=birth_date,
+                date_of_death=date_of_death
+            )
+
+            # Add to database
+            db.session.add(new_author)
+            db.session.commit()
+
+            flash('Author added successfully!', 'success')
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Error adding author: {str(e)}', 'error')
+
+    return render_template('add_author.html')
