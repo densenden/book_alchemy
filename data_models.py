869b217e9988624a43ca391ff02689a812efd77c
densenden <git@deniskreuzer.dk>
2025-03-10 16:28:59 +0100

    book and author classes, empty lib

---
+++ data_models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
+
+    def __str__(self):
+        return f"Author: {self.name}, Born: {self.birth_date}, Died: {self.date_of_death if self.date_of_death else 'N/A'}"
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(13), nullable=False, unique=True)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+    def __str__(self):
+        return f"Book: {self.title}, ISBN: {self.isbn}, Published: {self.publication_year}, Author ID: {self.author_id}"
+
+
+with app.app_context():
+  db.create_all()
